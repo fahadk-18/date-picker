0667101e282045da29a40e85a86d4cc621cd447c
Fahad Khan <fahad91939@gmail.com>
2023-10-08 21:30:07 -0400

    create variable for date-picker element and add event listener to toggle class

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 const datePickerButton = document.querySelector('.date-picker-button')
+const datePicker = document.querySelector('.date-picker')
+
+datePickerButton.addEventListener("click", e => {
+    e.preventDefault()
+    datePicker.classList.toggle('.show')
+} )
